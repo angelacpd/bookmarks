5fd67b6b397c11bf77430e35458254c21c33e223
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-06-23 09:47:21 -0300

    Created user registration view.

--- account/urls.py
+++ account/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Register user
+    path('register/', views.register, name='register'),
 ]
