22194851e7ceefed9cfb03f8ba881a9647f18796
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-06-28 15:53:17 -0300

    Created many-to-many relationships between users and images.

--- images/models.py
+++ images/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='images_created',
@@ -12,6 +13,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def __str__(self):
         return self.title
